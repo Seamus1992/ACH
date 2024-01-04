0cd00b9dfd3c90e9b17c28100adfe7045eca298a
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-01-04 13:31:24 +0100

    Update App.py

--- App.py
+++ App.py
@@ -3124,7 +3124,8 @@ if username == valid_username and password == valid_password:
             hold = 'Horsens U19'
             df = df[df['label'].str.contains(hold)]
             df['date'] = df['date'].apply(lambda x: parser.parse(x).strftime('%d-%m-%Y'))
-            df = df.sort_values(by='date')            
+            df = df.sort_values(by='date')
+            st.dataframe(df)            
             valgtekamp = st.multiselect('Vælg kamp', df['label'].unique(),default=df['label'].unique()[0])
             df.loc[df['player.id'] == 624663, 'player.name'] = 'Je. Beluli'
             df.loc[df['pass.recipient.id'] == 624663, 'pass.recipient.name'] = 'Je. Beluli'

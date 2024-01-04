698591ab841d4b434a47c91d31301f893f0bcc57
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-01-04 10:05:23 +0100

    Update Datascouting.py

--- Datascouting.py
+++ Datascouting.py
@@ -3893,7 +3893,6 @@ def Anden_division ():
         st.write(df_Angriberesæsonen)
     df = pd.read_csv(r'xT/2nd Division 23 24.csv')
 
-    df = df[['id','matchId','label','date','matchPeriod','minute','shot.isGoal','type.primary','type.secondary','location.x','location.y','team.name','opponentTeam.name','player.id','player.name','pass.accurate','pass.endLocation.x','pass.endLocation.y','pass.recipient.id','pass.recipient.name','possession.id','possession.duration','possession.id','possession.eventsNumber','possession.eventIndex','possession.team.name','possession.attack.xg','carry.progression','carry.endLocation.x','carry.endLocation.y']]
     df1 = df.copy()
     df = df[(df['pass.accurate'] ==True) | (df['carry.progression'] > 0)]
     df = df[~df['type.primary'].str.contains('infraction')]
@@ -3994,7 +3993,7 @@ def Anden_division ():
     xT['xT'] = xT['xT'].fillna(0)
     xT['xT Rank'] = xT['xT'].rank(ascending=False).astype(int)
 
-    xgc = df1[['id','label','team.name','player.id','player.name','possession.attack.xg','possession.id','type.primary','type.secondary']]
+    xgc = df1
     xgchold = xgc.drop_duplicates(subset='possession.id',keep='first')
     xgchold = xgchold.rename(columns={'possession.attack.xg': 'Hold xG'})
     xgchold = xgchold.groupby('team.name')['Hold xG'].agg('sum').reset_index()
@@ -4398,7 +4397,6 @@ def Tredje_division ():
         st.write(df_Angriberesæsonen)
     df = pd.read_csv(r'xT/3. Division 23 24.csv')
 
-    df = df[['id','matchId','label','date','matchPeriod','minute','shot.isGoal','type.primary','type.secondary','location.x','location.y','team.name','opponentTeam.name','player.id','player.name','pass.accurate','pass.endLocation.x','pass.endLocation.y','pass.recipient.id','pass.recipient.name','possession.id','possession.duration','possession.id','possession.eventsNumber','possession.eventIndex','possession.team.name','possession.attack.xg','carry.progression','carry.endLocation.x','carry.endLocation.y']]
     df1 = df.copy()
     df = df[(df['pass.accurate'] ==True) | (df['carry.progression'] > 0)]
     df = df[~df['type.primary'].str.contains('infraction')]
@@ -4499,7 +4497,7 @@ def Tredje_division ():
     xT['xT'] = xT['xT'].fillna(0)
     xT['xT Rank'] = xT['xT'].rank(ascending=False).astype(int)
 
-    xgc = df1[['id','label','team.name','player.id','player.name','possession.attack.xg','possession.id','type.primary','type.secondary']]
+    xgc = df1
     xgchold = xgc.drop_duplicates(subset='possession.id',keep='first')
     xgchold = xgchold.rename(columns={'possession.attack.xg': 'Hold xG'})
     xgchold = xgchold.groupby('team.name')['Hold xG'].agg('sum').reset_index()

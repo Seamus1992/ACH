f411d2ac68662e685bad9b41e69544dcd9a82c07
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-01-04 14:48:39 +0100

    Update App.py

--- App.py
+++ App.py
@@ -2373,7 +2373,7 @@ if username == valid_username and password == valid_password:
                 y = xgplacering['location.y']
                 player_names = xgplacering['player.name']  # Extract player names
                 label_text = xgplacering.sort_values(by='possession.attack.xg',ascending=False)
-                label_text = xgplacering[['player.name', 'possession.attack.xg']].to_string(index=False)
+                label_text = label_text[['player.name', 'possession.attack.xg']].to_string(index=False)
                 label_text = label_text.replace('possession.attack.xg','xG')
                 shot_xg = xgplacering['possession.attack.xg'].astype(float)
                 min_size = 5  # Minimum dot size

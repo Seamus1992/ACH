7b47a08ddf4136396ece1481d8a6a52c0a616951
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-01-05 12:41:54 +0100

    Update App.py

--- App.py
+++ App.py
@@ -7324,6 +7324,7 @@ if username == valid_username and password == valid_password:
             with col1:
                 st.write("Pasninger")
                 average_passes_df.columns = ['Antal']
+                average_passes_df = average_passes_df.round(2)
                 st.dataframe(average_passes_df)
 
             with col2:

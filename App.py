29763e2a2dcc7d31d152be1657fc5771d546b915
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-01-08 15:06:15 +0100

    Update App.py

--- App.py
+++ App.py
@@ -2392,6 +2392,7 @@ if username == valid_username and password == valid_password:
                                 
             team_passes = ((df['type.primary'] == 'pass') | (df['carry.progression'] != '')) & (df['team.name'] == hold) & (df['type.secondary'] != "Throw-in")
             team_passes = df.loc[team_passes, ['location.x', 'location.y', 'pass.endLocation.x', 'pass.endLocation.y', 'player.name','player.id','pass.recipient.name','pass.recipient.id','pass.accurate','carry.progression','carry.endLocation.y','carry.endLocation.x']]
+            st.dataframe(team_passes)
             players = team_passes[['player.id','player.name']]
             players = players.drop_duplicates()
             pitch = Pitch(pitch_type='wyscout',line_color='white', pitch_color='#02540b', pad_top=20)

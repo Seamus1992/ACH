d2221ec871a97ecfa7ea04472683588df678fbfd
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-01-08 15:09:30 +0100

    Update App.py

--- App.py
+++ App.py
@@ -2415,6 +2415,15 @@ if username == valid_username and password == valid_password:
                         ax.arrow(x, y, dx, dy, color='red', length_includes_head=True, head_width=1, head_length=0.8)
                         pitch.scatter(player_df['location.x'][i], player_df['location.y'][i], color='red', ax=ax)
 
+                    if player_df['carry.progression'][i] != '':
+                        carry_end_x = player_df['carry.endLocation.x'][i]
+                        carry_end_y = player_df['carry.endLocation.y'][i]
+                        dx_carry = carry_end_x - x
+                        dy_carry = carry_end_y - y
+                        ax.arrow(x, y, dx_carry, dy_carry, color='yellow', length_includes_head=True, head_width=1, head_length=0.8)
+                        pitch.scatter(carry_end_x, carry_end_y, color='yellow', ax=ax)
+
+
             st.title('Pasninger')
             st.pyplot(fig)
 

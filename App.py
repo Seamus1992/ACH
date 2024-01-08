600327f57ec5e3316f566a7bd5088c5b48a01f8f
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-01-08 15:05:30 +0100

    Update App.py

--- App.py
+++ App.py
@@ -2390,8 +2390,8 @@ if username == valid_username and password == valid_password:
                 st.write('Xg plot (Jo større markering, jo større xG)')
                 st.pyplot(plt.gcf(), use_container_width=True)
                                 
-            team_passes = (df['type.primary'] == 'pass') & (df['team.name'] == hold) & (df['type.secondary'] != "Throw-in")
-            team_passes = df.loc[team_passes, ['location.x', 'location.y', 'pass.endLocation.x', 'pass.endLocation.y', 'player.name','player.id','pass.recipient.name','pass.recipient.id','pass.accurate']]
+            team_passes = ((df['type.primary'] == 'pass') | (df['carry.progression'] != '')) & (df['team.name'] == hold) & (df['type.secondary'] != "Throw-in")
+            team_passes = df.loc[team_passes, ['location.x', 'location.y', 'pass.endLocation.x', 'pass.endLocation.y', 'player.name','player.id','pass.recipient.name','pass.recipient.id','pass.accurate','carry.progression','carry.endLocation.y','carry.endLocation.x']]
             players = team_passes[['player.id','player.name']]
             players = players.drop_duplicates()
             pitch = Pitch(pitch_type='wyscout',line_color='white', pitch_color='#02540b', pad_top=20)

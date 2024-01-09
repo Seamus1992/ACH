df3e541e68c06ef6793d59d1e24f98eb6eeaaeac
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-01-09 11:35:25 +0100

    Update App.py

--- App.py
+++ App.py
@@ -2891,8 +2891,9 @@ if username == valid_username and password == valid_password:
             players = players.drop_duplicates()
 
             combined_df = pd.concat([team_passes, team_dribbles])
-            st.dataframe(combined_df)
-            # Plotting
+            for i in combined_df.index:
+                if combined_df['player.name'][i] == combined_df['pass.recipient.name'][i]:
+                    combined_df.loc[i, 'pass.accurate'] = False            # Plotting
             pitch = Pitch(pitch_type='wyscout', line_color='white', pitch_color='#02540b', pad_top=20)
             fig, axs = pitch.grid(ncols=4, nrows=5, grid_height=0.85, title_height=0.00, axis=False, title_space=0.04, endnote_space=0.01)
             plt.figure()

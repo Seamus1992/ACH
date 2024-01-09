87fd555033afeb1dc5b5b9997a21347796b564b4
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-01-09 11:40:45 +0100

    Update App.py

--- App.py
+++ App.py
@@ -2896,6 +2896,7 @@ if username == valid_username and password == valid_password:
                 False,
                 combined_df['pass.accurate']
             )
+            st.dataframe(combined_df)
             pitch = Pitch(pitch_type='wyscout', line_color='white', pitch_color='#02540b', pad_top=20)
             fig, axs = pitch.grid(ncols=4, nrows=5, grid_height=0.85, title_height=0.00, axis=False, title_space=0.04, endnote_space=0.01)
             plt.figure()

322f84c12efc3cbc8115662ad3b500feedbd239f
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-01-09 10:51:55 +0100

    Update App.py

--- App.py
+++ App.py
@@ -2437,8 +2437,8 @@ if username == valid_username and password == valid_password:
                     dy_pass = player_df['carry.endLocation.y'][i] - player_df['location.y'][i]
 
                     if player_df['carry.progression'][i] != None:
-                        ax.arrow(x, y, dx_pass, dy_pass, color='#0dff00', length_includes_head=True, head_width=1, head_length=0.8)
-                        pitch.scatter(player_df['location.x'][i], player_df['location.y'][i], color='#0dff00', ax=ax)
+                        ax.arrow(x, y, dx_pass, dy_pass, color='yellow', length_includes_head=True, head_width=1, head_length=0.8)
+                        pitch.scatter(player_df['location.x'][i], player_df['location.y'][i], color='yellow', ax=ax)
 
             st.title('Driblinger')
             st.pyplot(fig)
